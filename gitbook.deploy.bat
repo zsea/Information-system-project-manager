0e571fe01e965c8bf9cf3d46521b405a84280df7
曾大海 <zsea@qq.com>
2019-09-05 11:44:42 +0800

    Updates

--- gitbook.deploy.bat
+++ gitbook.deploy.bat
@@ -1,3 +1,10 @@
 ::该脚本自动部署到gh-pages分支
 ::需要安装gh-pages，npm install -g gh-pages
-gh-pages -d _book
+@echo off
+chcp 65001
+echo 正在编译...
+call gitbook build
+echo 正在发布...
+call gh-pages -d _book
+echo 发布成功
+@echo on
